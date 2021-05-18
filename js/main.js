89724a5ed158e8293e27cac8fdfcd830af0d9f9c
YakDm79 <gav1@ya.ru>
2021-05-18 16:48:08 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -32,3 +32,9 @@ const reviewSlider = new Swiper('.reviews-slider', {
   },
   
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
+});
