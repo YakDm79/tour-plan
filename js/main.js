3cd75e59dc5894ff8e9a8ae20df70718e799054c
YakDm79 <gav1@ya.ru>
2021-05-24 16:36:48 +0300

    create: validate form

--- js/main.js
+++ js/main.js
@@ -1,82 +1,113 @@
 $(document).ready(function () {
-  const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  
-  loop: true,
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-    hideOnClick: false,
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  
-});
-const reviewSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  
-  loop: true,
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-    hideOnClick: false,
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+      hideOnClick: false,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  const reviewSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
-});
-  
-  var modalButton = $('[data-toggle=modal]');
-  var closeModalButton = $('.modal__close');
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal);
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+      hideOnClick: false,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
 
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass('modal__overlay--visible');
-    modalDialog.addClass('modal__dialog--visible');
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
   }
 
   function closeModal(event) {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass('modal__overlay--visible');
-    modalDialog.removeClass('modal__dialog--visible');
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
   }
   function closeModalKey(event) {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass('modal__overlay--visible');
-    modalDialog.removeClass('modal__dialog--visible');
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
   }
-  	// Закрытие по клавише Esc.
-	$(document).keydown(function(e) {
-		if (e.keyCode === 27) {
-		    closeModalKey();
-		}
+  // Закрытие по клавише Esc.
+  $(document).keydown(function (e) {
+    if (e.keyCode === 27) {
+      closeModalKey();
+    }
   });
-    // Закрытие модального окна по клику за его пределами  
+  // Закрытие модального окна по клику за его пределами
   $(document).mouseup(function (e) {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    if (!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0){
-    closeModalKey();
+    if (!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0) {
+      closeModalKey();
     }
+  });
+  // Обработка форм
+  $('.phone').mask('0-000-000-0000');
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: {
+          required: true,
+          minlength: 14,
+        },
+      },
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: jQuery.validator.format("At least 2 characters required"),
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please, enter your phone number",
+          minlength: jQuery.validator.format("Example number: 1-234-567-1111"),
+        },
+      },
+    });
   });
 });
